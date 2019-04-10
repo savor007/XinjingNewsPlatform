e889041f25236ded206bc1a336dce5c605856355
印妈妈 <yinqiaoyin@itcast.cn>
2019-04-10 16:53:31 +0800

    update news detail loading

--- source/modules/news/views.py
+++ source/modules/news/views.py
@@ -1,8 +1,26 @@
+from source.utility.response_code import RET
 from . import news_blueprint
-from flask import render_template, current_app
+from flask import render_template, current_app, jsonify
+from source.models import *
+
 
 
 @news_blueprint.route('/<int:news_id>')
 def Function_Loadnews(news_id):
     current_app.logger.debug(news_id)
-    return render_template('news/detail.html')
+    news=None
+    try:
+        news=News.query.filter(News.id==news_id).first()
+    except Exception as error:
+        current_app.logger.error(error)
+        return jsonify(errno=RET.DBERR, errmsg= "database accessing error")
+    else:
+        if not news:
+            return jsonify(errno=RET.NODATA, errmsg="can not find the news by id")
+        else:
+            news.clicks+=1
+            data={
+                "news":news.to_dict(),
+
+            }
+            return render_template('news/detail.html', data=data)
